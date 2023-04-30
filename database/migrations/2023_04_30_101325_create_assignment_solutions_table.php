0c90f01bdb2c0e57dc75bbe72449c3b2a69272b0
Adrian Somor <adrian.somor2@gmail.com>
2023-04-30 12:15:53 +0200

    feat: create-migrations

---
+++ database/migrations/2023_04_30_101325_create_assignment_solutions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assignment_solutions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('assignment_id')->constrained();
+            $table->string('solution_file_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assignment_solutions');
+    }
+};
