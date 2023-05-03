f991bf5a408088eb0fcebfdbd20a30c3760dcffd
Adrian Somor <adrian.somor2@gmail.com>
2023-05-03 07:39:16 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// *** Resource Routes *** !!!
+// Common Resource Routes:
+// index - Show all items
+// show - Show single item
+// create - Show form to create new item
+// store - Store new item
+// edit - Show form to edit item
+// update - Update item
+// destroy - Delete item  
+
 Route::get('/', function () {
     return view('welcome');
 });
