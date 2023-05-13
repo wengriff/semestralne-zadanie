f15bf172178eeb07b2d50b71d83cc9775d9349bd
Ľuboš Likó <goodlike0807@gmail.com>
2023-05-13 12:24:54 +0200

    images+ teacher fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -24,24 +24,31 @@ class Controller extends BaseController
     public function dashboard()
     {
         $indexResult = $this->index();
-        $role = auth()->user()->role;
+        $role=null;
+        if(auth()->check()) {
+            $role = auth()->user()->role;
+        
 
-    
-        if ($role == 'student') {
-            $mathProblems = auth()->user()->mathProblems()->with('assignmentSet')->get();
-            $assignmentsGroupedBySet = $mathProblems->groupBy(function ($item, $key) {
-            return $item->assignmentSet->id;
-    });
+        
+            if ($role == 'student') {
+                $mathProblems = auth()->user()->mathProblems()->with('assignmentSet')->get();
+                $assignmentsGroupedBySet = $mathProblems->groupBy(function ($item, $key) {
+                return $item->assignmentSet->id;
+        });
 
-    return $indexResult->with([
-        'assignments' => $mathProblems,  // If you still need the $assignments variable in the view
-        'assignmentsGroupedBySet' => $assignmentsGroupedBySet
-    ]);
-    } else if ($role == 'teacher') {
-        $assignmentSetsResult = (new AssignmentController())->assignmentSets();
-        $indexResult->with('assignmentSets', $assignmentSetsResult->getData()['assignmentSets']);
-    }
+        return $indexResult->with([
+            'assignments' => $mathProblems,  // If you still need the $assignments variable in the view
+            'assignmentsGroupedBySet' => $assignmentsGroupedBySet
+        ]);
+        } else if ($role == 'teacher') {
+            $assignmentSetsResult = (new AssignmentController())->assignmentSets();
+            $indexResult->with('assignmentSets', $assignmentSetsResult->getData()['assignmentSets']);
+        }
+
     return $indexResult;
+}else{
+    return view('auth.register');
+}
 }
 
 
